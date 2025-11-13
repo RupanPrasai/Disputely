304d33b8e48ed9295ca8757c9bbbdddc805be8a3
Rupan Prasai <rupanprasai18@gmail.com>
2025-11-12 22:44:17 -0600

    update

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from "path";
 
-// https://vite.dev/config/
+
+const r = (p: string) => resolve(__dirname, p);
+
 export default defineConfig({
+  build: {
+    outDir: "dist",
+    emptyOutDir: false,
+
+    rollupOptions: {
+      input: {
+        background: r("background/index.ts"),
+        content: r("content/index.ts")
+      },
+      output: {
+        entryFileNames: (chunk) => {
+          if (chunk.name === "background") return "background/index.js";
+          if (chunk.name === "content") return "content/index.js";
+          return "[name].js";
+        }
+      },
+    },
+  },
   plugins: [react()],
 })
