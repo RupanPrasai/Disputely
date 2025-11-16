a5d76ace564cc09c056794dee248c6baca1befea
Rupan Prasai <rupanprasai18@gmail.com>
2025-11-15 19:56:24 -0600

    update

--- client/src/pages/overlay/index.tsx
+++ client/src/pages/overlay/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import { createRoot, Root } from 'react-dom/client';
 import Overlay from '@pages/overlay/Overlay';
 import '@pages/overlay/index.css';
 
-const init = () => {
+let reactRoot: Root | null = null;
+
+export function mountOverlay() {
   if (document.getElementById('__extension_overlay_root')) {
     return;
   }
@@ -11,13 +13,30 @@ const init = () => {
   const overlayContainer = document.createElement('div');
   overlayContainer.id = '__extension_overlay_root';
 
-  overlayContainer.style.all = 'initial';
 
   document.body.appendChild(overlayContainer);
 
   const root = createRoot(overlayContainer);
   root.render(<Overlay />);
-};
+}
 
+export function unmountOverlay() {
+  const overlayContainer = document.getElementById('__extension_overlay_root');
+  if (!overlayContainer) {
+    return;
+  }
 
-init();
+  reactRoot?.unmount();
+  reactRoot = null;
+
+  overlayContainer.remove();
+}
+
+export function toggleOverlay() {
+  if (document.getElementById('__extension_overlay_root')) {
+    unmountOverlay();
+  } else {
+    mountOverlay();
+  }
+}
+
